25b243ab3f8408eccc072351befbc4ff26a0fe87
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-05-08 14:39:00 +0300

    feat(Projects): Add content int he Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -80,10 +80,10 @@ export const Projects = () => {
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <p></p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <p>Each project in my portfolio represents not only my technical expertise but also my commitment to delivering solutions that exceed client expectations. Whether it's building responsive websites, developing intuitive web applications, or enhancing user experiences, I take pride in my ability to transform ideas into impactful digital solutions that drive success.</p>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
